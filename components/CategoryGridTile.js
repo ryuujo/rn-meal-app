d46a13a684b2f4adacddd3d8309398798c182e38
unknown <mirukusheki@gmail.com>
2020-01-13 18:07:38 +0700

    overflow problem fix

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -34,7 +34,9 @@ const styles = StyleSheet.create({
     margin: 10,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden"
+    elevation: 5,
+    overflow:
+      Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible'
   },
   container: {
     flex: 1,
@@ -43,7 +45,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 10,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end'
